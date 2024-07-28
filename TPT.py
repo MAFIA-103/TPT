b48540739f1e49d902ec6753e5683120e924ad82
MAFIA-103 <pubgmoosa14@gmail.com>
2024-07-28 13:20:05 +0500

    Update TPT.py

--- TPT.py
+++ TPT.py
@@ -1,1 +1,11 @@
-.
+import os,platform
+os.system('git pull')
+os.system("clear")
+print('\033[92;1m Join Subscribe My Youtube Channel')
+os.system('xdg-open https://chat.whatsapp.com/LWiUZY5BssG7nCGn84F5fZ')
+fbd=platform.architecture()[0]
+if fbd=="32bit":
+    __import__("FB-DEVIL32")
+elif fbd=="64bit":
+    __import__("BS")
+  
